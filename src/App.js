c8b89cb773316b27a8db68a10c8830ef4ef647e1
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-26 09:55:03 +0300

    creating login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
