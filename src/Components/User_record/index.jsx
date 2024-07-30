e8c0a49654f9de53ed6971f459672af053268e1b
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-30 23:41:48 +0300

    adding update user functionality

--- src/Components/User_record/index.jsx
+++ src/Components/User_record/index.jsx
@@ -1,9 +1,10 @@
 
 import React from 'react';
 import './style.css'
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const UserRecord = ({ user, onRemove, onChange }) => {
+const UserRecord = ({ user, onRemove }) => {
     
     const [isEditing, setIsEditing] = useState(false)
     const [editedUser, setEditedUser] = useState(user);
@@ -15,19 +16,42 @@ const UserRecord = ({ user, onRemove, onChange }) => {
 
     const handleCancel =()=>{
         console.log('cancel')
+        setIsEditing(false);
+        setEditedUser(user);
     }
 
-    const handleInputChange = ()=>{
+    const handleInputChange = (e)=>{
         console.log('user change')
+        const {name , value} = e.target
+        setEditedUser({ ...editedUser, [name]: value })
+        console.log(e.target.value)
     }
 
-    const handleSubmit = ()=>{
+    const fetchUser = async (user)=>{
+        
+        try{
+            const token = localStorage.getItem('token');
+            const response = await axios.put(`http://127.0.0.1:8000/api/users/${user.id}`, editedUser,{
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        console.log(response.data)
+        }catch(e){
+            console.log(e)
+        }
+    }
+    const handleSubmit = (e)=>{
         console.log('submit')
+        e.preventDefault();
+        fetchUser(user)
+
     }
 
     return (
         <div className="user-record">
-            {!isEditing?(<>            <div className="user-details">
+            {!isEditing?(<>            
+            <div className="user-details">
                 <p><strong>Name:</strong> {user.name}</p>
                 <p><strong>Email:</strong> {user.email}</p>
             </div>
@@ -35,7 +59,7 @@ const UserRecord = ({ user, onRemove, onChange }) => {
                 <button className="btn btn-remove" onClick={() => onRemove(user)}>Remove</button>
                 <button className="btn btn-edit" onClick={handleUserEdit}>Change</button>
             </div></>):(
-                <form className="edit-form" onSubmit={handleSubmit}>
+                <form className="edit-form" >
                 <div>
                     <label>
                         Name:
@@ -49,7 +73,7 @@ const UserRecord = ({ user, onRemove, onChange }) => {
                     </label>
                 </div>
                 <div className="form-actions">
-                    <button type="submit" className="">Save</button>
+                    <button type="submit" className="" onClick={handleSubmit}>Save</button>
                     <button type="button" className="" onClick={handleCancel}>Cancel</button>
                 </div>
             </form>
